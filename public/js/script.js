8f4bb81d9d3f0dce7930030f4a244c42f835ad00
Jared Eklin <jaredeklin@gmail.com>
2018-05-21 11:20:50 -0600

    Add POST and DELETE functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,39 @@
 
 
 $('.add-photo-btn').on('click', addPhoto)
-$('.delete-btn').on('click', deletePhoto)
+$('.display-photos').on('click', '.delete-btn', deletePhoto)
 
 getPhotos()
 
-function addPhoto(event) {
+async function addPhoto(event) {
   event.preventDefault()
 
+  const photo = {
+    title: $('.title-input').val(),
+    url: $('.url-input').val()
+  }
 
+  const response = await fetch('/api/v1/photos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(photo)
+  })
 
-  console.log("click")
+  const photoData = await response.json();
+
+  $('.display-photos').append(`
+      <article class="photo-card" data-id=${photoData.id}>
+        <img class="image-output" src=${photoData.url}>
+        <h5 class="title-output">${photoData.title}</h5>
+        <button class="delete-btn">x</button>
+      </article>
+    `)
 }
 
 async function getPhotos() {
+  $('.display-photos').empty()
 
   const response = await fetch('/api/v1/photos')
   const photos = await response.json()
@@ -31,6 +51,16 @@ async function getPhotos() {
   })
 }
 
-function deletePhoto() {
-  
+async function deletePhoto() {
+  const id = $(this).parent().data('id')
+
+  const response = await fetch(`/api/v1/photos/${id}`, {
+    method: 'DELETE'
+  })
+
+  // const deleteData = await response.json()
+
+  // console.log(deleteData)
+
+  $(this).parent().remove()
 }
