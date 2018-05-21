6d21f838e656e9f05f54c99ef320ca6276090445
Jared Eklin <jaredeklin@gmail.com>
2018-05-21 12:11:36 -0600

    Add GET test and setup seeding in test env

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -28,4 +28,39 @@ describe('Client Routes', () => {
 
 describe('API Routes', () => {
 
+  beforeEach(() => {
+    database.migrate.rollback()
+      .then(() => {
+        database.migrate.latest()
+      })
+      .then(() => {
+        return database.seed.run()
+      })
+  })
+
+  it('should GET all the photos', (done) => {
+    chai.request(app)
+      .get('/api/v1/photos')
+      .end((error, response) => {
+        response.should.be.json
+        response.should.have.status(200)
+        response.body.should.be.an('array')
+        response.body.length.should.equal(3)
+        response.body[0].should.have.property('title', 'pic1')
+        response.body[0].should.have.property('url', 'https://wallpaperbrowse.com/media/images/3848765-wallpaper-images-download.jpg')
+        response.body[0].should.have.property('id', 1)
+        response.body[2].should.have.property('title', 'pic3')
+        response.body[2].should.have.property('url', 'https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg')
+        response.body[2].should.have.property('id', 3)
+        done()
+      })
+  })
+
+  it('should POST a photo to the database', () => {
+    chai.request(app)
+      .get('/api/v1/photos')
+      .end((error, response) => {
+        
+      })
+  })
 });
