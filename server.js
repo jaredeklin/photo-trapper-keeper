cb993326b7ef9ab06d2a3e5c6469ad51684dbd78
Jared Eklin <jaredeklin@gmail.com>
2018-05-21 10:05:34 -0600

    Add GET, POST and DELETE endpoints

--- server.js
+++ server.js
@@ -15,7 +15,25 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 
+app.get('/api/v1/photos', (request, response) => {
+  database('photos').select()
+    .then(photos => response.status(200).json(photos))
+    .catch(error => response.status(404).json('Path not found'))
+})
 
+app.post('/api/v1/photos', (request, response) => {
+  const photo = request.body;
+
+  database('photos').insert(photo, ['title', 'url'])
+    .then(photo => response.status(201).json(photo[0]))
+    .catch(error => response.status(422).json(error))
+})
+
+app.delete('/api/v1/photos/:id', (request, response) => {
+  database('photos').where('id', request.params.id).del()
+    .then(photo => response.status(204).json(request.params.id))
+    .catch(error => response.status(404).json(error))
+})
 
 
 
